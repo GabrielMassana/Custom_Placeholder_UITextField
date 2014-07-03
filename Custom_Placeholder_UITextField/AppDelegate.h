e9d38aab00f3ad612cb2d06a2d95b4cd808e0546
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-07-03 10:13:15 +0100

    Initial Commit

---
+++ Custom_Placeholder_UITextField/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Custom_Placeholder_UITextField
+//
+//  Created by Gabriel Massana on 03/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Custom_Placeholder_UITextField/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Custom_Placeholder_UITextField
+//
+//  Created by Gabriel Massana on 03/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
